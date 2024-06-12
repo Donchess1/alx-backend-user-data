c964238cece1363c9d26bcfad194366edbe70ca6
Donchess1 <Stepabod@yahoo.com>
2024-06-12 19:54:24 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__user__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
