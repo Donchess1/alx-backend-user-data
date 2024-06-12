6bdb3101d6e0fb0fa64de483c77453b282309f35
Donchess1 <Stepabod@yahoo.com>
2024-06-12 19:46:36 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 """ declarative base"""
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+app = Flask(__user__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(250), nullable=False)
+    session_id = db.Column(db.String(250), nullable=True)
+    reset_token = db.Column(db.String(250), nullable=True)
 
-class User(Base):
-    """ User model """
+# Create the database and tables
+with app.app_context():
+    db.create_all()
 
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+if __name__ == '__main__':
+    app.run(debug=True)
